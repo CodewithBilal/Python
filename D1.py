ee14c69a611f1aef612a885f775ec0951b75f487
Cri8tivity <bilalijaz0107@gmail.com>
2023-08-30 20:25:04 +0500

    Create D1.py

---
+++ D1.py
@@ -0,0 +1,9 @@
+# University=UCP
+# Degree=Sofware Enineering
+print("My name is Bilal.I am a software engineer now I am doing pyhton task")
+
+# now break the upper line into multiple lines
+print("My name is Bilal.\nI am a Software Engineer.\nNow, I am doing pyhton task")
+
+# now put software engineering into string""
+print("My name is Bilal.\nI am a \"Software Engineer\"\nNow, I am doing pyhton task")
