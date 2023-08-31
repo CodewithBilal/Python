b1f778bad679bf419a6a3290b675c6078fc6f92a
Cri8tivity <bilalijaz0107@gmail.com>
2023-08-31 18:26:59 +0500

    Create D2.py

---
+++ D2.py
@@ -0,0 +1,21 @@
+
+# Python Simple calculator
+a=5
+b=2
+# For addition
+print('Adition of ',a,' and',b ,' is =',a+b,'\n')
+
+# For Subtraction
+print('Subtraction of ',a,' and',b ,' is =',a-b,'\n')
+
+# For Multiplication
+print('Multiplication of ',a,' and',b ,' is =',a*b,'\n')
+
+# For Division
+print('Division of ',a,' and',b ,' is =',a/b,'\n')
+
+# For Modulus
+print('Modulus of ',a,' and',b ,' is =',a%b,'\n')
+
+# For floor Division
+print('Division of ',a,' and',b ,' is =',a//b,'\n')
